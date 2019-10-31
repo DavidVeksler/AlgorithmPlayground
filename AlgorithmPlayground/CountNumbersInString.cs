b88d8317e50109ab5c0fc49405d26670d87fd7fc
David Veksler <heroic@gmail.com>
2019-10-31 14:49:27 -0400

    public int CountNumbersInString(string s)

---
+++ AlgorithmPlayground/CountNumbersInString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace AlgorithmPlayground
+{
+    public class CountNumbers
+    {
+        public int CountNumbersInString(string s)
+        {
+            int total = 0;
+            string cn = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Convert.ToInt32(s[i]) > 0)
+                {
+                    cn += s[i];
+                }
+                else
+                {
+                    total += Convert.ToInt32(cn);
+                    cn = "";
+                }
+            }
+            return total;
+        }
+    }
+}
+
